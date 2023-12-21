c43abdcaba3c435b9ac7ccee8212e1589d74dea5
Gubio Garcia <gubiogarcia@gmail.com>
2023-12-21 13:35:47 -0300

    Feat: Área administrativa / admin.py

--- coreAplication/models.py
+++ coreAplication/models.py
@@ -5,3 +5,7 @@ class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em Estoque')
+
+    def __str__(self):
+       # return f'{self.nome} {self.estoque}' concatenação de parâmetros para serem apresentados
+        return self.nome
